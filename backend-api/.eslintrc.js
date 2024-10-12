e3c3e975acb10c0ffa7ea0ebda9998fad5af385f
ngothuythanhtam <ntthanhtam.forwork@gmail.com>
2024-10-12 21:34:39 +0700

    Configure eslint and prettier

--- backend-api/.eslintrc.js
+++ backend-api/.eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
     env: {
-    node: true,
-    commonjs: true,
-    es2022: true,
+        node: true,
+        commonjs: true,
+        es2022: true,
     },
     extends: ['eslint:recommended', 'prettier'],
 };
