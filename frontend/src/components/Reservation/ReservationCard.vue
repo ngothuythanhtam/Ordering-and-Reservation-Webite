f995b76b9634bce03eed75c767baa80d7ca9a3f7
ngothuythanhtam <ntthanhtam.forwork@gmail.com>
2024-11-08 11:00:12 +0700

    Add Reservation Page in staff role

---
+++ frontend/src/components/Reservation/ReservationCard.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    reservation: { type: Object, required: true }
+});
+
+// Hàm định dạng thời gian
+function formatDate(dateString) {
+    const date = new Date(dateString);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // tháng trong JavaScript bắt đầu từ 0
+    const year = date.getFullYear();
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0'); // lấy giây và đảm bảo có 2 chữ số
+
+    return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+}
+
+function formatDateUse(dateString) {
+    const date = new Date(dateString);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // tháng trong JavaScript bắt đầu từ 0
+    const year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+}
+
+</script>
+
+<template>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">{{ reservation.reservation_id }}</h5>
+            <p class="card-text">ID khách hàng: {{ reservation.userid }}</p>
+            <p class="card-text">ID bàn: {{ reservation.table_id }}</p>
+            <p class="card-text">Thời gian sử dụng: {{ formatDateUse(reservation.reservation_date) }}</p>
+            <p class="card-text">Yêu cầu: {{ reservation.special_request }}</p>
+            <p class="card-text">Thời gian đặt: {{ formatDate(reservation.create_at) }}</p>
+            <p class="card-text">Trạng thái: {{ reservation.status }}</p>
+        </div>
+    </div>
+</template>
---
+++ frontend/src/components/Reservation/ReservationList.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+    reservations: { type: Array, default: () => [] },
+    selectedIndex: { type: Number, default: -1 },
+});
+const $emit = defineEmits(['update:selectedIndex']);
+</script>
+
+<template>
+    <ul class="list-group">
+        <li class="list-group-item px-3" v-for="(reservation, index) in reservations"
+            :class="{ active: index === selectedIndex }" :key="reservation.reservation_id"
+            @click="$emit('update:selectedIndex', index)">
+            {{ reservation.reservation_id }}
+        </li>
+    </ul>
+</template>
